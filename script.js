5525e071c9dd897108aa0c556cfad3049df70c9b
Radan Olga <plotnica@yahoo.com>
2018-04-18 17:23:12 +0300

    keeping learning

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Student = /** @class */ (function () {
     function Student(firstName, middleInitial, lastName) {
         this.firstName = firstName;
@@ -7,12 +17,12 @@ var Student = /** @class */ (function () {
     }
     return Student;
 }());
-function greeter(person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-var user = new Student("Jane", "M.", "User");
-console.log(greeter(user));
-console.log(user.fullName);
+// function greeter(person : Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName;
+// }
+// let user = new Student("Jane", "M.", "User");
+// console.log(greeter(user));
+// console.log(user.fullName)
 var Animals = /** @class */ (function () {
     function Animals(name, type, numberOfLegs) {
         this.name = name;
@@ -53,3 +63,117 @@ var c = Color.Blue;
 console.log(c);
 var colorName = Color[2];
 console.log(colorName);
+colorName = Color[3];
+console.log(colorName);
+function createSquare(config) {
+    var newSquare = { color: "white", area: "fgfgh", circle: 23 };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width + "" + config.width;
+    }
+    // return newSquare;
+    console.log("This is a function that does something ,it display the color of the Square,with is: " + newSquare.circle + " ");
+}
+createSquare({ color: "black", width: 300 });
+var mySearch;
+mySearch = function (source, subString) {
+    var result = source.search(subString);
+    return result > -1;
+};
+console.log(mySearch("eu merg la mare la ora 7", "7"));
+var Greeter = /** @class */ (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("world");
+console.log(greeter.greeting);
+console.log("I am saying hello from the meton " + greeter.greet());
+// class Animal {
+//     move(distanceInMeters: number=0) {
+//         console.log(`Animal moved ${distanceInMeters}m.`);
+//     }
+// }
+// class Dog extends Animal {
+//     bark() {
+//         console.log('Woof! Woof!');
+//     }
+// }
+// const dog = new Dog();
+// dog.bark();
+// dog.move(100);
+// dog.bark();
+// class Animal {
+//     name: string;
+//     constructor(theName: string) { this.name = theName; }
+//     move(distanceInMeters: number = 0) {
+//         console.log(`${this.name} moved ${distanceInMeters}m.`);
+//     }
+// }
+// class Snake extends Animal {
+//     constructor(name: string) { super(name); }
+//     move(distanceInMeters = 5) {
+//         console.log("Slithering...");
+//         super.move(distanceInMeters);
+//     }
+// }
+// class Horse extends Animal {
+//     constructor(name: string) { super(name); }
+//     move(distanceInMeters = 45) {
+//         console.log("Galloping...");
+//         super.move(distanceInMeters);
+//     }
+// }
+// let sam = new Snake("Sammy the Python");
+// let tom: Animal = new Horse("Tommy the Palomino");
+//  sam.move();
+//  tom.move(34);
+var Animal = /** @class */ (function () {
+    function Animal(theName) {
+        this.name = theName;
+    }
+    return Animal;
+}());
+var Rhino = /** @class */ (function (_super) {
+    __extends(Rhino, _super);
+    function Rhino() {
+        return _super.call(this, "Rhino") || this;
+    }
+    return Rhino;
+}(Animal));
+var Employee = /** @class */ (function () {
+    function Employee(theName) {
+        this.name = theName;
+    }
+    return Employee;
+}());
+var animall = new Animal("Goat");
+var rhino = new Rhino();
+var employee = new Employee("Bob");
+console.log(animall);
+console.log(rhino);
+console.log(employee);
+animall = rhino;
+//animall = employee;
+var Grid = /** @class */ (function () {
+    function Grid(scale) {
+        this.scale = scale;
+    }
+    Grid.prototype.calculateDistanceFromOrigin = function (point) {
+        var xDist = (point.x - Grid.origin.x);
+        var yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    };
+    Grid.origin = { x: 0, y: 0 };
+    return Grid;
+}());
+var grid1 = new Grid(1.0); // 1x scale
+var grid2 = new Grid(5.0); // 5x scale
+console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+console.log(grid2.calculateDistanceFromOrigin({ x: 10, y: 10 }));
--- script.ts
+++ script.ts
@@ -10,14 +10,14 @@ interface Person {
     lastName: string;
 }
 
-function greeter(person : Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
+// function greeter(person : Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName;
+// }
 
-let user = new Student("Jane", "M.", "User");
+// let user = new Student("Jane", "M.", "User");
 
-console.log(greeter(user));
-console.log(user.fullName)
+// console.log(greeter(user));
+// console.log(user.fullName)
 
 class Animals{
     constructor(public name:string, public type:string, public numberOfLegs:number){}
@@ -64,3 +64,147 @@ var c:Color=Color.Blue
 console.log(c)
 let colorName:string=Color[2]
 console.log(colorName)
+colorName=Color[3]
+console.log(colorName)
+
+
+
+interface SquareConfig {
+    color?: string;
+    width?: number;
+}
+interface Square{
+    color:string;
+    area:any;
+    circle:number;
+}
+
+function createSquare(config: SquareConfig):void{
+    let newSquare = {color: "white", area:"fgfgh",circle:23};
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width+""+config.width;
+    }
+    // return newSquare;
+    console.log(`This is a function that does something ,it display the color of the Square,with is: ${newSquare.circle} `)
+}
+createSquare({color:"black",width:300})
+
+// let mySquare = createSquare({color: "black",width:200});
+// console.log(mySquare);
+
+interface SearchFunc {
+    (source: string, subString: string):boolean;
+}
+
+let mySearch: SearchFunc;
+mySearch = function(source: string, subString: string) {
+    let result = source.search(subString);
+    return result >-1;
+}
+
+console.log(mySearch("eu merg la mare la ora 7", "7"))
+
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let greeter = new Greeter("world");
+console.log(greeter.greeting)
+console.log("I am saying hello from the meton "+greeter.greet())
+
+// class Animal {
+//     move(distanceInMeters: number=0) {
+//         console.log(`Animal moved ${distanceInMeters}m.`);
+//     }
+// }
+
+// class Dog extends Animal {
+//     bark() {
+//         console.log('Woof! Woof!');
+//     }
+// }
+
+// const dog = new Dog();
+// dog.bark();
+// dog.move(100);
+// dog.bark();
+
+
+// class Animal {
+//     name: string;
+//     constructor(theName: string) { this.name = theName; }
+    
+//     move(distanceInMeters: number = 0) {
+//         console.log(`${this.name} moved ${distanceInMeters}m.`);
+//     }
+// }
+
+// class Snake extends Animal {
+//     constructor(name: string) { super(name); }
+//     move(distanceInMeters = 5) {
+//         console.log("Slithering...");
+//         super.move(distanceInMeters);
+//     }
+// }
+
+// class Horse extends Animal {
+//     constructor(name: string) { super(name); }
+//     move(distanceInMeters = 45) {
+//         console.log("Galloping...");
+//         super.move(distanceInMeters);
+//     }
+// }
+
+// let sam = new Snake("Sammy the Python");
+// let tom: Animal = new Horse("Tommy the Palomino");
+
+//  sam.move();
+//  tom.move(34);
+
+ class Animal {
+    private name: string;
+    constructor(theName: string) { this.name = theName; }
+}
+
+class Rhino extends Animal {
+    constructor() { super("Rhino"); }
+}
+
+class Employee {
+    private name: string;
+    constructor(theName: string) { this.name = theName; }
+}
+
+let animall = new Animal("Goat");
+let rhino = new Rhino();
+let employee = new Employee("Bob");
+console.log(animall)
+console.log(rhino)
+console.log(employee)
+animall = rhino;
+//animall = employee;
+
+class Grid {
+    static origin = {x: 0, y: 0};
+    calculateDistanceFromOrigin(point: {x: number; y: number;}) {
+        let xDist = (point.x - Grid.origin.x);
+        let yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    }
+    constructor (public scale: number) { }
+}
+
+let grid1 = new Grid(1.0);  // 1x scale
+let grid2 = new Grid(5.0);  // 5x scale
+
+console.log(grid1.calculateDistanceFromOrigin({x: 10, y: 10}));
+console.log(grid2.calculateDistanceFromOrigin({x: 10, y: 10}));
